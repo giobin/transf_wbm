1f63f0bc0d0e89dc7e85a9061b74e3f0dff9075f
Giovanni <bonettagiovanni@gmail.com>
2023-05-31 15:57:46 +0200

    gymnasium in place of gym

--- src/envs/wrappers.py
+++ src/envs/wrappers.py
@@ -4,7 +4,7 @@ Credits to https://github.com/openai/baselines/blob/master/baselines/common/atar
 
 from typing import Tuple
 
-import gym
+import gymnasium as gym
 import numpy as np
 from PIL import Image
 
@@ -64,11 +64,11 @@ class NoopResetEnv(gym.Wrapper):
         if self.override_num_noops is not None:
             noops = self.override_num_noops
         else:
-            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1)
+            noops = self.unwrapped.np_random.integers(1, self.noop_max + 1)
         assert noops > 0
         obs = None
         for _ in range(noops):
-            obs, _, done, _ = self.env.step(self.noop_action)
+            obs, _, done, _, _ = self.env.step(self.noop_action)
             if done:
                 obs = self.env.reset(**kwargs)
         return obs
@@ -129,7 +129,7 @@ class MaxAndSkipEnv(gym.Wrapper):
         total_reward = 0.0
         done = None
         for i in range(self._skip):
-            obs, reward, done, info = self.env.step(action)
+            obs, reward, done, truncated, info = self.env.step(action)
             if i == self._skip - 2:
                 self._obs_buffer[0] = obs
             if i == self._skip - 1:
